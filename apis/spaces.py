e805a500835c1f6b64308aaa0ed061115c6425cf
Max <maximomacchi@gmail.com>
2019-03-27 23:17:20 -0700

    /spaces endpoint moved into own file as part of directory organization

---
+++ apis/spaces.py
@@ -0,0 +1,10 @@
+from flask_restplus import Namespace, Resource, fields
+from utilities import database_utilities
+
+api = Namespace("spaces", description="Information relating to spaces")
+
+
+@api.route('/')
+class Spaces(Resource):
+    def get(self):
+        return database_utilities.execute_query("SELECT * FROM spaces;")
