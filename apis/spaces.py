6044ea8f3fcd16c590fe8a73485f73ce0093f625
Max <maximomacchi@gmail.com>
2019-03-28 21:47:39 -0700

    Added GET method to /spaces/<space_id> for fetching specific space information

--- apis/spaces.py
+++ apis/spaces.py
@@ -7,4 +7,13 @@ api = Namespace("spaces", description="Information relating to spaces")
 @api.route('/')
 class Spaces(Resource):
     def get(self):
+        """ Fetch data for all spaces """
         return database_utilities.execute_query("select * from spaces")
+
+
+@api.route('/<string:space_id>')
+class Space(Resource):
+    def get(self, space_id):
+        """ Fetch data for space based on space_id """
+        return database_utilities.execute_query(
+            f"""select * from spaces where space_id = '{space_id}'""")
