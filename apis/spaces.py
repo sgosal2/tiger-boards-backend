b1af40077fce84a3e995eb9468128cce174307ce
Max <maximomacchi@gmail.com>
2019-03-31 18:58:05 -0700

    POST method added to api/spaces endpoint for inserting new spacex

--- apis/spaces.py
+++ apis/spaces.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_restplus import Namespace, Resource, fields
 from utilities import database_utilities
 
@@ -9,6 +10,14 @@ class Spaces(Resource):
     def get(self):
         """ Fetch data for all spaces """
         return database_utilities.execute_query("select * from spaces")
+
+    def post(self):
+        """ Insert data for new space """
+        query = f"""insert into spaces values (%s, %s, %s, %s, %s);"""
+        parameters = (request.form['space_id'], request.form['building_id'],
+                      request.form['name'], request.form['capacity'],
+                      request.form['features'])
+        database_utilities.execute_query(query, parameters)
 
 
 @api.route('/<string:space_id>')
