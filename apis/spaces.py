f40e4088118eaf927c21c85ee3fd7a2dc00bd095
Max <maximomacchi@gmail.com>
2019-03-31 20:54:22 -0700

    PATCH method added to api/spaces/space_id endpoint

--- apis/spaces.py
+++ apis/spaces.py
@@ -23,6 +23,21 @@ class Spaces(Resource):
 @api.route('/<string:space_id>')
 class Space(Resource):
     def get(self, space_id):
-        """ Fetch data for space based on space_id """
+        """ Fetch data for space with the corresponding space_id """
         return database_utilities.execute_query(
             f"""select * from spaces where space_id = '{space_id}'""")
+
+    def delete(self, space_id):
+        """ Deletes space with the corresponding space_id """
+        return database_utilities.execute_query(
+            f"""delete from spaces where space_id = `{space_id}`""")
+
+    def patch(self, space_id):
+        """ Replaces information of corresponding space_id with request body """
+        query = f"""update spaces set space_id = %s, building_id = %s, """
+        query += f"""name = %s, capacity = %s, features = %s """
+        query += f"""where space_id = '{space_id}'"""
+        parameters = (request.form['space_id'], request.form['building_id'],
+                      request.form['name'], request.form['capacity'],
+                      request.form['features'])
+        database_utilities.execute_query(query, parameters)
