a1638fee871cd70f7940cbf22f26e4ed7de21ac7
Nathan Chica <n_chica@u.pacific.edu>
2019-04-17 17:51:59 -0700

    modified buildings endpoint

--- apis/buildings.py
+++ apis/buildings.py
@@ -19,16 +19,17 @@ class Buildings(Resource):
         parameters = (json_data['building_id'], json_data['building_name'])
         execute_query(query, parameters)
 
-    def patch(self):
+@api.route('/<string:building_id>')
+class Building(Resource):
+    def patch(self, building_id):
         """Edit a building info."""
         query = "UPDATE building SET building_name = %s WHERE building_id = %s"
         json_data = request.get_json()
-        parameters = (json_data['new_name'], json_data['building_id'])
+        parameters = (json_data['new_name'], building_id)
         execute_query(query, parameters)
 
-    def delete(self):
+    def delete(self, building_id):
         """Delete a building"""
         query = "DELETE FROM building WHERE building_id = %s"
-        json_data = request.get_json()
-        parameters = (json_data['building_id'],)
+        parameters = (building_id,)
         execute_query(query, parameters)
