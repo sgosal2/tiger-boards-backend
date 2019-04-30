8c9e3d2e456110d6b21f4cf962f1fda3296bb86f
Max <maximomacchi@gmail.com>
2019-04-30 04:16:27 -0700

    Removed import statement for /favorites endpoint

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,7 +1,6 @@
 from flask_restplus import Api
 from .admins import api as admins_api
 from .buildings import api as buildings_api
-from .favorites import api as favorites_api
 from .login import api as login_api
 from .spaces import api as spaces_api
 from .users import api as users_api
@@ -15,7 +14,6 @@ api = Api(
 
 api.add_namespace(admins_api)
 api.add_namespace(buildings_api)
-api.add_namespace(favorites_api)
 api.add_namespace(login_api)
 api.add_namespace(spaces_api)
 api.add_namespace(users_api)
