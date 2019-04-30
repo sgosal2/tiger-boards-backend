df7749d8c741ad80d78d2e7a896efc3cd0a6087f
Max <maximomacchi@gmail.com>
2019-04-30 04:16:16 -0700

    Added /refresh endpoint to refresh JWT if it expires

--- apis/login.py
+++ apis/login.py
@@ -1,5 +1,8 @@
 from flask import request, jsonify
-from flask_jwt_extended import create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies
+from flask_jwt_extended import (
+    create_access_token, create_refresh_token, set_access_cookies,
+    set_refresh_cookies, jwt_refresh_token_required, get_jwt_identity
+)
 from flask_restplus import Namespace, Resource
 from utilities import database_utilities
 
@@ -24,6 +27,18 @@ class Login(Resource):
             resp = jsonify({"login": True})
             set_access_cookies(resp, access_token)
             set_refresh_cookies(resp, refresh_token)
-            return resp, 200
+            return resp
         else:
             return jsonify({"msg": "User is not an admin"})
+
+
+@api.route('/refresh')
+class Refresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        access_token = create_access_token(identity=current_user)
+
+        resp = jsonify({"refresh": True})
+        set_access_cookies(resp, access_token)
+        return resp
