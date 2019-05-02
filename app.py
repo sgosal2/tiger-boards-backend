e463432f1e6567077793282eecce558b1a13b123
Nathan Chica <n_chica@u.pacific.edu>
2019-05-01 17:34:15 -0700

    added more cors headers

--- app.py
+++ app.py
@@ -8,10 +8,14 @@ import psycopg2
 app = Flask(__name__)
 api.init_app(app)
 
+
 @app.after_request
 def add_cors_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = '*'
     return response
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
