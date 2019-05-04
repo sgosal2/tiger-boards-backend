ba64bc9648dd5c99cd9934ef7f167a94feba8ebe
Nathan Chica <n_chica@u.pacific.edu>
2019-05-04 00:10:01 -0700

    fixes in login endpoint

--- apis/login.py
+++ apis/login.py
@@ -1,4 +1,4 @@
-from flask import request, jsonify
+from flask import request, jsonify, make_response
 from flask_jwt_extended import (
     create_access_token, create_refresh_token, set_access_cookies,
     set_refresh_cookies, jwt_refresh_token_required, get_jwt_identity
@@ -14,22 +14,23 @@ class Login(Resource):
     def post(self):
         """ Returns JWT upon login verification """
         json_data = request.get_json()
-        if not json_data['email']:
-            return jsonify({"msg": "Missing email"}), 400
+        if 'email' not in json_data or not json_data['email']:
+            print("HERE")
+            return make_response(jsonify(msg="Missing email"), 401)
 
         data = database_utilities.execute_query(
-            f"""select * from admins where email = %s""", (json_data['email'], ))
+            "SELECT * FROM admins WHERE email = %s", (json_data['email'], ))
         if data:
             email = data[0]['email']
             access_token = create_access_token(identity=email)
             refresh_token = create_refresh_token(identity=email)
 
-            resp = jsonify({"login": True})
+            resp = jsonify(login=True, is_admin=True)
             set_access_cookies(resp, access_token)
             set_refresh_cookies(resp, refresh_token)
             return resp
         else:
-            return jsonify({"msg": "User is not an admin"})
+            return jsonify(msg="User is not an admin", is_admin=False)
 
 
 @api.route('/refresh')
